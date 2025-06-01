7de9e5651d4988b9fef3485f36cd5d27671ea517
Ahmad Zani Syechkar <ahmadzanisy@gmail.com>
2025-06-01 19:20:21 +0700

    update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex">
+      <nav className="flex justify-between p-10">
+        <h1>Hello, World!</h1>
+      </nav>
+    </div>
+  );
+}
